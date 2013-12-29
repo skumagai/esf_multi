4a176f73cd199462c51875bab308bc148af2b717
Seiji Kumagai <seiji.kumagai@gmail.com>
2013-12-28 21:26:26 -0500

    use cmake to build and test

--- test/afs_test.cc
+++ test/afs_test.cc
@@ -141,12 +141,3 @@ TEST_F(AFSTest, CheckSingleton) {
 
 
 }  // namespace
-
-
-int main(int argc, char **argv) {
-
-  ::testing::InitGoogleTest(&argc, argv);
-
-  return RUN_ALL_TESTS();
-
-}
--- test/allele_test.cc
+++ test/allele_test.cc
@@ -117,11 +117,3 @@ TEST_F(AlleleTest, AlleleCompare) {
 
 
 }  // namespace
-
-int main(int argc, char **argv) {
-
-  ::testing::InitGoogleTest(&argc, argv);
-
-  return RUN_ALL_TESTS();
-
-}
